0ee5d400ec0dd29586421d27b776e2830c71d76b
travelgeezer <travelgeezer@icloud.com>
2018-01-07 12:05:06 +0800

    Added test rsa route

--- response_data_format/service.py
+++ response_data_format/service.py
@@ -1,16 +1,35 @@
 # -*- coding: utf-8 -*-
 
 import json
+import base64
 from flask import Blueprint, request
-
+from user.utils import decrypt, private_key
 
 response_data_format = Blueprint('response_data_format', __name__)
 
 @response_data_format.route('/api/v1/response_data_format/<data>')
 def data_format(data):
+
     data = {
-        "code": 0,
+        "code": 200,
         "data": data,
+        "info": "success"
+    }
+
+    return json.dumps(data)
+
+
+@response_data_format.route('/api/v1/test_rsa/', methods=['POST'])
+def test_rsa():
+    data = request.get_json()
+    key = data.get('key')
+    print(key)
+    cipher = decrypt(private_key, base64.b64decode(key))
+    print('cipher: ', cipher)
+
+    data = {
+        "code": 200,
+        "data": cipher,
         "info": "success"
     }
 
